388b7bbc0e10a7c0b29131442df824afe0ebddbb
Igor Kling <klingiv@altarix.ru>
2019-04-07 17:05:09 +0400

    отображение прогресса поиска

--- src/renderer/app.js
+++ src/renderer/app.js
@@ -1,111 +1,133 @@
-const { ipcRenderer, remote } = window.require('electron')
-const { callMain, answerMain } = require('./ipc').default(ipcRenderer)
+import './app.css'
+
+// для того чтобы скрыть обводку фокусного элемента!!!
+// https://stackoverflow.com/questions/52814241/how-do-i-hide-the-border-around-blueprint-js-switch-component
+// https://blueprintjs.com/docs/#core/accessibility.focus-management
+import { FocusStyleManager } from "@blueprintjs/core";
+FocusStyleManager.onlyShowFocusOnTabs();
+
 import React, { Component } from "react";
 import { observer, inject } from "mobx-react";
-import uuidv4 from 'uuid/v4'
+
+
+import LinearProgress from '@material-ui/core/LinearProgress';
+import { withStyles } from '@material-ui/core/styles';
+
+import { Button, Checkbox, InputGroup, Intent } from "@blueprintjs/core";
+
+
+const Progress = withStyles({
+  container: {
+    flexGrow: 1
+  },
+  root: {
+    position: 'relative',
+    overflow: 'hidden',
+    height: 2,
+  },
+  linearColorPrimary: {
+    backgroundColor: '#428dc3',
+  },
+  linearBarColorPrimary: {
+    backgroundColor: '#1371b4',
+  },
+})(({ classes }) => (
+  <div className={classes.container}>
+    <LinearProgress classes={{
+      root: classes.root,
+      colorPrimary: classes.linearColorPrimary,
+      barColorPrimary: classes.linearBarColorPrimary,
+    }} />
+  </div>
+))
+
+
 @inject(({ store }) => ({ store }))
 @observer
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.hash = window.location.hash.replace("#", "");
-
-
-    ipcRenderer.on('search-result', (event, result) => {
-      console.log('search-result:', result)
-    })
-
-
-    ipcRenderer.on('search', (event, details) => {
-
-      console.log('DETAILS:', details)
-
-      const { folderPath, status, file, path, query, searchOptions, result } = details
-      if (status === 'ready') {
-        console.log('SEARCH READY')
-        return
-      }
-
-
-      if (Array.isArray(result) && result.length > 0) {
-        for (const { item: { line, text }, matches: [{ indices }] } of result) {
-          this.props.store.addSearchResult({
-            text,
-            line,
-            file,
-            path,
-            matches: indices
-          })
-        }
-      }
-    })
-
-  }
-
-  openFile = () => {
-    remote.dialog.showOpenDialog({
-      properties: ['openDirectory'],
-      title: 'Open Folder',
-      defaultPath: '/Users/kling/Altarix/MARM'
-    }, result => {
-      if (Array.isArray(result)) {
-        this.props.store.setProjectPath(result[0])
-      }
-    });
   }
 
   handleQueryChange = event => {
     this.props.store.setQuery(event.target.value)
   }
 
-  search = () => {
-    const { projectPath, query, searchOptions = {}, clearSearchResults } = this.props.store
-
-    clearSearchResults()
-
-    console.log(`searching '${query}' in ${projectPath} ...`)
-    const correlationMarker = uuidv4()
-    ipcRenderer.send('search', correlationMarker, projectPath, query, searchOptions)
+  handleCaseSesitiveChange = event => {
+    this.props.store.setCaseSensitive(event.target.checked)
   }
 
-  render() {
-    const { store: { projectPath, query, searchResult } } = this.props;
+  renderSearchResult = () => {
+    const { store: { searchResult } } = this.props;
 
-    if (!projectPath) {
-      return (
-        <button onClick={() => this.openFile()}>Select Folder</button>
-      )
+    if (!searchResult) {
+      return null
+    }
+
+    if (searchResult.length === 0) {
+      return <p>No results found.</p>
     }
 
     return (
-      <>
-        <input type="text" value={query} onChange={this.handleQueryChange} />
-        <button onClick={() => this.search()}>Search</button>
-        <ul>
-          {searchResult.map(({ text, line, file, path, matches }) => {
+      <ul>
+        {searchResult.map(({ text, line, file, path, matches }) => {
 
-            return (
-              <li key={`${path}/${file}`}>
-                <span>{file}</span>{"\u00a0"}<span>{path}</span>{"\u00a0"}<span style={{ backgroundColor: 'lightgreen', borderRadius: '50%', paddingLeft: '5px', paddingRight: '5px' }}>{matches.length}</span>
-                <ul>
-                  {matches.map(([first, last]) => {
-                    const beforeMatch = text.slice(0, first)
-                    const matched = text.slice(first, last + 1)
-                    const afterMatch = text.slice(last + 1)
-                    return (
-                      <li key={`${path}/${file}:${line}:${first}`} onClick={() => { console.log(`${path}/${file}:${line}:${first}`) }}>
-                        <span>{beforeMatch}</span>
-                        <span style={{ backgroundColor: 'yellow' }}>{matched}</span>
-                        <span>{afterMatch}</span>
-                      </li>
-                    )
-                  })}
-                </ul>
-              </li>
-            )
-          })}
-        </ul>
-      </>
+          return (
+            <li key={`${path}/${file}`}>
+              <span>{file}</span>{"\u00a0"}<span>{path}</span>{"\u00a0"}<span style={{ backgroundColor: 'lightgreen', borderRadius: '50%', paddingLeft: '5px', paddingRight: '5px' }}>{matches.length}</span>
+              <ul>
+                {matches.map(([first, last]) => {
+                  const beforeMatch = text.slice(0, first)
+                  const matched = text.slice(first, last + 1)
+                  const afterMatch = text.slice(last + 1)
+                  return (
+                    <li key={`${path}/${file}:${line}:${first}`} onClick={() => { console.log(`${path}/${file}:${line}:${first}`) }}>
+                      <span>{beforeMatch}</span>
+                      <span style={{ backgroundColor: 'yellow' }}>{matched}</span>
+                      <span>{afterMatch}</span>
+                    </li>
+                  )
+                })}
+              </ul>
+            </li>
+          )
+        })}
+      </ul>
+    )
+  }
+
+  render() {
+    const { store: { search, openFile, projectPath, query, searchInProgress, caseSensitive } } = this.props;
+
+    if (!projectPath) {
+      return (
+        <button onClick={openFile}>Select Folder</button>
+      )
+    }
+
+    const SearchButton = <Button icon="search" minimal={true} small={true} intent={Intent.PRIMARY} onClick={search} /> // arrow-right
+
+    return (
+      <div style={{ width: '200px' }}>
+        <InputGroup
+          // leftIcon="search"
+          onChange={this.handleQueryChange}
+          placeholder="Search"
+          rightElement={SearchButton}
+          small={true}
+          value={query}
+        />
+
+        <Checkbox
+          className="bp3-align-right"
+          label="Case Sensitive"
+          checked={caseSensitive}
+          onChange={this.handleCaseSesitiveChange}
+        />
+        {!!searchInProgress && <Progress />}
+        {this.renderSearchResult()}
+      </div >
     )
 
   }
--- src/renderer/store.js
+++ src/renderer/store.js
@@ -1,15 +1,22 @@
+const { ipcRenderer, remote } = window.require('electron')
+const { callMain, answerMain } = require('./ipc').default(ipcRenderer)
+import uuidv4 from 'uuid/v4'
 import { observable, action, computed, transaction, toJS, set } from "mobx";
 
 class Store {
 
+  @observable searchInProgress = false
+
   @observable projectPath = null;
 
-  @action.bound
-  setProjectPath(path) {
-    this.projectPath = path;
-  }
-
   @observable query = ''
+
+  @observable caseSensitive = false
+
+  @action.bound
+  setCaseSensitive(value) {
+    this.caseSensitive = value
+  }
 
   @observable searchOptions = {
     caseSensitive: true,
@@ -30,7 +37,7 @@ class Store {
     this.query = query
   }
 
-  @observable.ref searchResult = []
+  @observable.ref searchResult = null
   // {
   //   text: '  hello hello',
   //   line: 3,
@@ -51,16 +58,64 @@ class Store {
   //   ]
   // },
 
+  @action.bound
+  search = () => {
+    if (!this.query && this.query.length < 3) return
+
+    this.searchInProgress = true
+
+    this.clearSearchResults()
+
+    const correlationMarker = uuidv4()
+    ipcRenderer.send('search', correlationMarker, this.projectPath, this.query, this.searchOptions)
+  }
 
   @action.bound
   addSearchResult(result) {
     console.log('addSearchResult', result)
+    if (!this.searchResult) {
+      this.searchResult = []
+    }
     this.searchResult = [...this.searchResult, result]
   }
 
   @action.bound
   clearSearchResults() {
-    this.searchResult = []
+    this.searchResult = null
+  }
+
+  constructor() {
+    ipcRenderer.on('search', (event, details) => {
+      const { folderPath, status, file, path, query, searchOptions, result } = details
+      if (status === 'ready') {
+        this.searchInProgress = false
+        return
+      }
+
+      if (Array.isArray(result) && result.length > 0) {
+        for (const { item: { line, text }, matches: [{ indices }] } of result) {
+          this.addSearchResult({
+            text,
+            line,
+            file,
+            path,
+            matches: indices
+          })
+        }
+      }
+    })
+  }
+
+  openFile = () => {
+    remote.dialog.showOpenDialog({
+      properties: ['openDirectory'],
+      title: 'Open Folder',
+      defaultPath: '/Users/kling/Altarix/MARM'
+    }, result => {
+      if (Array.isArray(result)) {
+        this.projectPath = result[0]
+      }
+    });
   }
 
 }
